0cf25e6317fcabb46d8b86089730c3b7679813b1
Fabio Lopez <fa.lopez10@uniandes.edu.co>
2018-09-05 14:54:28 -0500

    i18n(es-en), about me in both langs

--- js/main.js
+++ js/main.js
@@ -5,23 +5,42 @@
 $(document).ready(main);
 
 function main(){
+	seti18n();
 	sobreMiDinamico();
 	lenguajeEscogido();
 	resaltarIntereses();
 }
 
-//funcion que juega con el texto en "sobre mi"
+//Function translating texts into preferred language for browser
+function seti18n(lang){
+	//get preffered language
+	if(!lang)
+		lang = navigator.language || navigator.userLanguage;
+	console.log("lang",lang);
+	// get json file for specific language and manipulate data
+	$.getJSON("../i18n/"+lang.substring(0,2)+".json",(data)=>{
+		for(let propt in data){
+    	let elements = $("."+propt);
+    	if(!elements)	continue;
+    	// change text for each element matching the class
+    	elements.text(data[propt]);
+
+		}
+	});
+}
+
+//function highlighting text in "about me" section
 function sobreMiDinamico(){
-	$("#about > .my-auto > p.mb-5").hover(function(){
+	$("#about-me > .my-auto > p.mb-5").hover(function(){
     $(this).css("font-weight", "Bold");
   }, function(){
     $(this).css("font-weight", "Normal");
   });
 }
-//funcion que muestra el lenguaje en "habilidades"
+//function showing programming language/tool in "skills"
 function lenguajeEscogido(){
 	$("#lenguaje-escogido").hide();
-	$("#habilidades > div > ul.list-inline.list-icons > .list-inline-item > i").hover(function(){
+	$("#skills > div > ul.list-inline.list-icons > .list-inline-item > i").hover(function(){
 		//guardamos el nombre del lenguaje
     var texto = $(this).attr("class");
 		var lenguaje = texto.split("-")[1];
